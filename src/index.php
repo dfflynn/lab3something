232b6e2cc53b974ce86129e8d9ceac0a0f77cddb
David Flynn <daflynn@csumb.edu>
2018-09-21 20:50:52 +0000

    fixing deck code

--- src/index.php
+++ src/index.php
@@ -1,81 +1,61 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <title>SilverJack </title>
+    </head>
+    <body>
 
-<head>
-    <meta char="utf-8" />
-    
-</head>
 
 
-<body>
-    <?php 
-    
-    $card = array("image" => "../img/scd/mist.png");
-    $card["suit"] = "clubs"; 
-    $card["value"] = 10;
-    
-    
-    foreach($card as $key => $value) {
-        echo "$key: $value";
-        echo "<br/>";
-    }
+
+
+    <?php
     
     $deck = array("numCards" => 52, "cards" => array());
     
     $deck["cards"][] = array();
-    $deck["cards"][0]["suit"] = "clubs";
     
-    echo $deck["cards"][0]["suit"];
+
     
     
+    for($t = 1;$t <= 52;$t++) {
+        
+    if($t <= 13 ) {
+        $deck["cards"][$t]["value"] = $t;
+        $deck["cards"][$t]["suit"] = "clubs";
+    }
+    else if($t > 13 && $t <= 26 ) {
+        $deck["cards"][$t]["value"] = $t - 13;
+        $deck["cards"][$t]["suit"] = "diamonds";
+    }
+    else if($t > 26 && $t <= 39 ) {
+        $deck["cards"][$t]["value"] = $t - 26;
+        $deck["cards"][$t]["suit"] = "hearts";
+    }
+    else if($t > 39 && $t <= 52 ) {
+        $deck["cards"][$t]["value"] = $t - 39;
+        $deck["cards"][$t]["suit"] = "spades";
+    }
+    
+    
+    
+        
+    
+        
+    }
+    //echo  $deck["cards"][1]["value"];
+    printall($deck);
+    
+    function printall($deckArray) {
+        foreach($deckArray['cards'] as $result) {
+            echo $result['value'], " ", $result['suit'], '<br>';
+        }
+    }
     
     
     ?>
+        
+        
     
-</body>
-
-
-
+    </body>
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
